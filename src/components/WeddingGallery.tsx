64d4bd3c263bb052f5851428db7be4c40accbb8c
TaiNguyen297 <tainguyen29702@gmail.com>
2024-12-03 17:07:38 +0700

    pop up image wedding gallery

--- src/components/WeddingGallery.tsx
+++ src/components/WeddingGallery.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-// Import JSON
+import React, { useState } from "react";
 import galleryData from "../utils/gallery.json";
 import styles from "../styles/Gallery.module.css";
 
 const WeddingGallery = () => {
+  const [selectedImage, setSelectedImage] = useState(null); 
+  const [isPopupOpen, setIsPopupOpen] = useState(false); 
+
+  const handleImageClick = (image: any) => {
+    setSelectedImage(image);
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedImage(null);
+    setIsPopupOpen(false);
+  };
+
   return (
     <section className={styles.weddingGallery}>
       <div className={styles.container}>
@@ -13,11 +25,27 @@ const WeddingGallery = () => {
       </div>
       <div className={styles.galleryContainer}>
         {galleryData.images.map((image, index) => (
-          <div key={index} className={styles.galleryItem}>
-            <img src={image} className={styles.galleryImage} />
+          <div
+            key={index}
+            className={styles.galleryItem}
+            onClick={() => handleImageClick(image)} 
+          >
+            <img src={image} alt={`Wedding Image ${index}`} className={styles.galleryImage} />
           </div>
         ))}
       </div>
+
+      {/* Popup */}
+      {isPopupOpen && (
+        <div className={styles.popupOverlay} onClick={handleClosePopup}>
+          <div className={styles.popupContent} onClick={(e) => e.stopPropagation()}>
+            <button className={styles.closeButton} onClick={handleClosePopup}>
+              &times;
+            </button>
+            <img src={selectedImage || ''} alt="Selected Wedding" className={styles.popupImage} />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
