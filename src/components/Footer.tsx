2dd5e30627a28f44c8f6a59c320195eb7bbbb4a2
TaiNguyen297 <tainguyen29702@gmail.com>
2025-01-17 15:04:27 +0700

    add font family

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import Image from "next/image";
 
 const Footer: React.FC = () => {
-
   return (
-    <footer className="bg-white py-12">
-      <div className="container mx-auto text-center">
+    <div className="bg-gray-300 ">
+      <div className="flex flex-col items-center justify-center h-screen">
         {/* Circle Image with Decorative Border */}
         <div className="relative w-40 h-40 sm:w-48 sm:h-48 mx-auto mb-6">
           <Image src="/images/LIN00151.JPG" alt='Thank you' width={400} height={300} layout="responsive"/>
@@ -16,9 +15,9 @@ const Footer: React.FC = () => {
         </div>
 
         {/* Thank You Text */}
-        <h2 className="text-3xl font-bold text-gray-700 italic">Thank you!</h2>
+        <h2 className="font-bold text-gray-700">Thank you!</h2>
       </div>
-    </footer>
+    </div>
   );
 };
 
